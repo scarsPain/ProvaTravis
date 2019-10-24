47c92bac52d6a2bfee7747f856d1a5b3195e60cf
scarsPain <34248798+scarsPain@users.noreply.github.com>
2019-10-24 10:32:52 +0200

    prova3

---
+++ src/com/HelloWorld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.HelloWorld;
+
+/* The HelloWorld class implements an application that displays 'Hello World!'to the standard output */
+
+
+public class HelloWorld {
+  public static void main (String[] args){
+    // Display " Hello World ! "
+    System.out.println("Hello World!");
+  }
+}
